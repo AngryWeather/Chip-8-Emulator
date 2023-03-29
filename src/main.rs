7245c255ece6a5bf29aa75bd109e78bb8aac6615
Weronika <WerRic96@protonmail.com>
2023-03-29 23:47:17 +0200

    Store bytes value in v register

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
 use std::env;
 use std::io;
 use std::fs::File;
@@ -11,16 +12,20 @@ use std::io::Seek;
 
 fn main() -> io::Result<()>{
 
-    // let sdl_context = sdl2::init().unwrap();
-    // let video_subsystem = sdl_context.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-    // let window = video_subsystem.window("chip-8 emulator", 64, 32)
-    //     .position_centered()
-    //     .build()
-    //     .unwrap();
+    let window = video_subsystem.window("chip-8 emulator", 64, 32)
+        .position_centered()
+        .build()
+        .unwrap();
 
-    // let mut event_pump = sdl_context.event_pump().unwrap();
-    // let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    let creator = canvas.texture_creator();
+    let mut texture = creator
+       .create_texture_target(PixelFormatEnum::RGB24, 64, 32).unwrap();
+
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
@@ -34,30 +39,27 @@ fn main() -> io::Result<()>{
     
     // Chip-8 puts programs in memory at 0x200
 
-    let mut chip8 = Chip8State::new([0;1024*4], [0;1024*4], 
-        [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
-        0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf], 0x0, 0x0, 0x0);
-
-    
+    let mut chip8 = Chip8State::new([0;1024*4], [0; 64 * 32], 
+        [0; 16], 0x0, 0x0, 0x0);    
 
     chip8.memory[0x200 .. (0x200 + &buffer.len())].copy_from_slice(&buffer[..]);
-    // chip8.screen[0xf00 .. chip8.memory.len()].copy_from_slice(&chip8.memory);
+    // texture.update(None, &chip8.screen, 8);
 
-    // 'running: loop {
-    //     canvas.set_draw_color(Color::RGB(255,255,255));
-    //     canvas.clear();
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit{..} => break 'running,
-    //             _ => {},
-    //         }
-    //     }
-    //     canvas.present();
-    // }
+    'running: loop {
+        canvas.set_draw_color(Color::RGB(255,255,255));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit{..} => break 'running,
+                _ => {},
+            }
+        }
+        canvas.present();
+    }
 
     // Read.
     while (chip8.pc) < 0x200 + buffer.len() as u16{
-        disassemble(&chip8.memory, &chip8);
+        disassemble(&mut chip8);
         chip8.pc += 2;
         print!("\n");
     }
@@ -101,11 +103,11 @@ struct Chip8State {
     delay: u8,
     sound: u8,
     memory: [u8; 1024 * 4],
-    screen: [u8; 1024 * 4],
+    screen: [u8; 64 * 32],
 }
 
 impl Chip8State {
-    fn new(memory: [u8; 1024 * 4], screen: [u8; 1024 * 4], 
+    fn new(memory: [u8; 1024 * 4], screen:  [u8; 64 * 32], 
         v: [u8;16], i: u16, delay: u8, sound: u8) -> Self {Chip8State {
             memory,
             screen,
@@ -119,10 +121,10 @@ impl Chip8State {
     }
 }
 
-fn disassemble(code_buffer: &[u8; 4096], chip8: &Chip8State) {
+fn disassemble(chip8: &mut Chip8State) {
     let pc = chip8.pc as usize;
-    let code0 = &code_buffer[pc];
-    let code1 = &code_buffer[pc + 1];
+    let code0 = &chip8.memory[pc];
+    let code1 = &chip8.memory[pc + 1];
     let first_nib = code0 >> 4;
 
     print!("{:x} {:x} {:x} ", pc, code0, code1);
@@ -142,7 +144,11 @@ fn disassemble(code_buffer: &[u8; 4096], chip8: &Chip8State) {
         0x03 => print!("{:-10} V{:01x},#${:02x}", "SKIP.EQ", code0 & 0xf, code1),
         0x04 => print!("{:-10} V{:01x},#${:02x}", "SKIP.NE", code0 & 0xf, code1),
         0x05 => print!("{:-10} V{:01x},V{:01x}", "SKIP.EQ", code0 & 0xf, code1 >> 4),
-        0x06 => print!("{:-10} V{:01x},#${:02x}", "MVI", code0 & 0xf, code1),
+        0x06 => {
+            print!("{:-10} V{:01x},#${:02x}", "MVI", code0 & 0xf, code1);
+            chip8.v[(code0 & 0xf) as usize] = *code1;
+            println!("\n{:0x?}", chip8.v);
+        },
         0x07 => print!("{:-10} V{:01x},#{:02x}", "ADI", code0 & 0xf, code1),
         0x08 => {
             let last_nib: u8 = code1 & 0xf;
@@ -161,6 +167,7 @@ fn disassemble(code_buffer: &[u8; 4096], chip8: &Chip8State) {
         }
         0x09 => print!("{:-10} V{:01x},V{:01x}", "SKIP.NE", code0 & 0xf, code1 & 0x0f),
         0x0a => {
+            chip8.i = 0x22a;
             let address_i: u8 = code0 & 0x0f;
             print!("{:-10} I,#${:01x}{:02x}", "MVI", address_i, code1);
         },
